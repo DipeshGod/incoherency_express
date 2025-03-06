cb38842b83e39b21d7564c24cde409d5c0bf0076
dipace7 <codeict.dipesh@gmail.com>
2025-03-06 20:53:05 +0545

    think more about it

--- src/alertRules.ts/schema.ts
+++ src/alertRules.ts/schema.ts
@@ -1,15 +1,20 @@
 import { Type } from "@sinclair/typebox";
 import { SupportedVersion } from "../apiMapping";
 
-export const AlertRulesSchema1 = Type.Object({
-  version: SupportedVersion,
-  id: Type.String(),
-});
+export const AlertRulesSchema1 = Type.Object(
+  {
+    name: Type.String(),
+  },
+  { $id: "AlertRulesSchema1" }
+);
 
-export const AlertRulesSchema2 = Type.Object({
-  version: SupportedVersion,
-  id: Type.String(),
-});
+export const AlertRulesSchema2 = Type.Object(
+  {
+    name: Type.String(),
+    duration: Type.Number(),
+  },
+  { $id: "AlertRulesSchema2" }
+);
 
 export const AlertRuleApiMapping: Record<SupportedVersion, unknown> = {
   "7.6.0": AlertRulesSchema1,
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+import Fastify from "fastify";
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { AlertRulesSchema1, AlertRulesSchema2 } from "./alertRules.ts/schema";
+
+const fastify = Fastify().withTypeProvider<TypeBoxTypeProvider>();
+
+fastify.post<{ Body: unknown }>(
+  "/api/:version/alertRules",
+  async (request, reply) => {
+    const { version }: any = request.params;
+
+    // Handle the valid request
+    return { message: "Alert rule created successfully", data: request.body };
+  }
+);
+
+fastify.listen({ port: 3000 });
