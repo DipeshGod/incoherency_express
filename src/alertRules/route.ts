02114a8137b2e8214fde68066e199da1c68c7e48
dipace7 <codeict.dipesh@gmail.com>
2025-03-06 21:40:34 +0545

    good starting point

---
+++ src/alertRules/route.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { Value } from "@sinclair/typebox/value";
+import { SupportedVersion } from "../apiMapping";
+import { AlertRulesSchema1, AlertRulesSchema2 } from "./schema";
+
+const router = Router();
+
+function validate(schema: any) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const validateResult = Value.Check(schema, req.body);
+    if (!validateResult) {
+      return res.status(400).json({ error: "Invalid request body" });
+    }
+    next();
+  };
+}
+
+function getSchemaForVersion(version: SupportedVersion) {
+  switch (version) {
+    case "7.6.0":
+      return AlertRulesSchema1;
+    case "7.7.0":
+      return AlertRulesSchema2;
+    default:
+      throw new Error("Unsupported version");
+  }
+}
+
+router.post("/:version", (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const version = req.params.version as SupportedVersion;
+    const schema = getSchemaForVersion(version);
+    validate(schema)(req, res, next);
+    res.status(200).json({ message: "Success" });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+export { router as alertRulesRouter };
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,12 @@
-import express, { Request, Response, NextFunction } from "express";
-
-import { Value } from "@sinclair/typebox/value";
-import { AlertRulesSchema1, AlertRulesSchema2 } from "./alertRules.ts/schema";
-import { SupportedVersion } from "./apiMapping";
+import express from "express";
+import { alertRulesRouter } from "./alertRules/route";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-function validate(schema: any) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const validateResult = Value.Check(schema, req.body);
-    if (!validateResult) {
-      return res.status(400).json({ error: "Invalid request body" });
-    }
-    next();
-  };
-}
-
-// Function to select schema based on version
-function getSchemaForVersion(version: SupportedVersion) {
-  switch (version) {
-    case "7.6.0":
-      return AlertRulesSchema1;
-    case "7.7.0":
-      return AlertRulesSchema2;
-    default:
-      throw new Error("Unsupported version");
-  }
-}
-
 // Route handler
-app.post(
-  "/alertRules/:version",
-  (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const version = req.params.version as SupportedVersion;
-      const schema = getSchemaForVersion(version);
-      validate(schema)(req, res, next);
-      res.status(200).json({ message: "Success" });
-    } catch (error: any) {
-      res.status(400).json({ error: error.message });
-    }
-  }
-);
+
+app.use("/AlertRules", alertRulesRouter);
 
 // Start the server
 app.listen(PORT, () => {
